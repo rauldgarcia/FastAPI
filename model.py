fe597f2e2006fccf06844f468cca2cca12806f36
raul <rauld.garcia95@gmail.com>
2025-11-24 14:39:29 -0600

    Add an example schema

--- model.py
+++ model.py
@@ -3,3 +3,11 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     id: int
     item: str
+
+    class Config:
+        Schema_extra ={
+            "Example": {
+                "id": 1,
+                "item": "Example schema"
+            }
+        }
