8ff6082f8d357f859ca8e7154255eb4d483310b8
raul <rauld.garcia95@gmail.com>
2025-11-28 17:07:49 -0600

    [Feature] Incorporate a SQL database and implement CRUD operations

--- planner/models/events.py
+++ planner/models/events.py
@@ -1,22 +1,41 @@
-from pydantic import BaseModel
-from typing import List
+from sqlmodel import JSON, SQLModel, Field, Column
+from typing import Optional, List
 
-class Event(BaseModel):
-    id: int
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     title: str
     image: str
     description: str
-    tags: List[str]
+    tags: List[str] = Field(sa_column=Column(JSON))
     location: str
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_schema_extra = {
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.jpg",
+                "description": "We will be discussing the contents of the FastAPI book in this event.",
+                "tags": ["python", "fastapi"],
+                "location": "Google Meet"
+            }
+        }
+
+
+class EventUpdate(SQLModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
 
     class Config:
         json_schema_extra = {
             "example": {
-                "id": 0,
                 "title": "FastAPI Book Launch",
-                "image": "https://linktomyimage.com/image.png",
-                "description": "We will be discussin the contents of the FastAPI book in this event.",
-                "tags": ["python", "fastapi", "book", "launch"],
+                "image": "https://linktomyimage.com/image.jpg",
+                "description": "We will be discussing the contents of the FastAPI book in this event.",
+                "tags": ["python", "fastapi"],
                 "location": "Google Meet"
             }
         }
