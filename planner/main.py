b8cf7fe82f22604376ae83d5101a32b058a383fe
raul <rauld.garcia95@gmail.com>
2025-12-03 14:54:57 -0600

    CORS are added

--- planner/main.py
+++ planner/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.users import user_router
 from routes.events import event_router
 import uvicorn
 from database.connection import Settings
 from contextlib import asynccontextmanager
+
+origins = ["*"]
 
 settings = Settings()
 @asynccontextmanager
@@ -14,6 +17,13 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
